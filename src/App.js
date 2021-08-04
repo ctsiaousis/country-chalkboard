473d16bcb90ce11c8e41911ad53584bbdf2d3e42
ctsiaousis <ctsiaous@gmail.com>
2021-08-05 01:43:07 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from './img/map.svg';
 import './App.css';
+import DocumentTitle from 'react-document-title'
 
 function App() {
   return (
+    <DocumentTitle title={"Country Chalkboard"}>
     <div className="App">
       <header className="App-header">
         <h1>Hello world</h1>
-        <h1>This site is under construction!</h1>
+        <h3>This site is under construction!</h3>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
+    </DocumentTitle>
   );
 }
 
